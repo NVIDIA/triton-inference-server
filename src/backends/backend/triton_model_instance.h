triton-inference-server/server

Use non-core TensorRT backend
Use vector instead of set

Use non-core TensorRT backend

Remove the core TRT backend implementation

[New Threading Model] For Single Instance, Dynamic Batcher, Execution… (#2863)

* [New Threading Model] For Single Instance, Dynamic Batcher, Execution Blocking

* Using comments instead of #if 0 for better readability

* Addressing review comments

[Rate Limiter] Single Instance, Exec blocking: Integrate rate limiter with dynamic b… (#2878)

* Single Instance, Exec blocking: Integrate rate limiter with dynamic batch scheduler

* Add notification

* Adressing review comments

Support multiple instances with TritonBackendThread. Fix dynamic sche… (#2891)

* Support mulitple instances with TritonBackendThread. Fix dynamic scheduler

* Fixing paranthesis

Add support for device blocking backend execution policy (#2894)

* [New Threading Model] Add support for device blocking backend execution policy

* Fix comments and message format

Enable WarmUp with new re-organization (#2896)

Fix some synchronization points and prevent extraneous notifications to scheduler thread (#2923)

* Enable WarmUp with new re-organization

* Reuse payload objects and fix some synchronization points

* Revert payload reuse and isssue cond-var notifications only when waiter is expected

Revert dynamic batcher

New design for better performance of no batching case with multiple i… (#2991)

* New design for better performance of no batching case with multiple instances

* Address review comments

* Fixup

* Add preserve ordering in without batching case

* Add instance specific queues

Build dynamic batches using BatcherThread (#3024)

* Build dynamic batches using BatcherThread

* Build payloads remptively into the payload queue

* Soe clean-ups and exposing transition to READY state to batcher

Integrate SequenceBatcher with new threading abstraction (#3057)

* Integrate SequenceBatcher with new threading abstraction

* Using default nice for batcher threads

* 

src/backends/backend/triton_model_instance.h
@@ -25,17 +25,23 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
+#include <functional>
+#include <future>
 #include <memory>
 #include <string>
+#include <thread>
 #include "model_config.pb.h"
 #include "src/core/constants.h"
+#include "src/core/memory.h"
 #include "src/core/metric_model_reporter.h"
 #include "src/core/server_message.h"
 #include "src/core/status.h"
+#include "triton/common/sync_queue.h"
 
 namespace nvidia { namespace inferenceserver {
 
 class TritonModel;
+class InferenceRequest;
 
 //
 // Represents a model instance.
@@ -44,7 +50,7 @@ class TritonModelInstance {
  public:
   static Status CreateInstances(
       TritonModel* model, const HostPolicyCmdlineConfigMap& host_policy_map,
-      const inference::ModelConfig& model_config);
+      const inference::ModelConfig& model_config, const bool device_blocking);
   ~TritonModelInstance();
 
   const std::string& Name() const { return name_; }
@@ -59,7 +65,13 @@ class TritonModelInstance {
   bool IsPassive() const { return passive_; }
   const std::vector<std::string>& Profiles() const { return profile_names_; }
 
-  TritonModel* Model() { return model_; }
+  Status Initialize() { return Status::Success; }
+  Status WarmUp();
+  void Schedule(
+      std::vector<std::unique_ptr<InferenceRequest>>&& requests,
+      const std::function<void()>& OnCompletion);
+
+  TritonModel* Model() const { return model_; }
   void* State() { return state_; }
   void SetState(void* state) { state_ = state; }
 
@@ -67,7 +79,7 @@ class TritonModelInstance {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(TritonModelInstance);
-
+  class TritonBackendThread;
   TritonModelInstance(
       TritonModel* model, const std::string& name, const size_t index,
       const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
@@ -79,7 +91,55 @@ class TritonModelInstance {
       const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
       const std::vector<std::string>& profile_names, const bool passive,
       const std::string& host_policy_name,
-      const HostPolicyCmdlineConfig& host_policy);
+      const HostPolicyCmdlineConfig& host_policy,
+      const inference::ModelRateLimiter& rate_limiter_config,
+      const bool device_blocking,
+      std::map<uint32_t, std::shared_ptr<TritonBackendThread>>*
+          device_to_thread_map);
+  Status SetBackendThread(
+      const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
+      const bool device_blocking,
+      std::map<uint32_t, std::shared_ptr<TritonBackendThread>>*
+          device_to_thread_map);
+  Status GenerateWarmupData();
+
+  void Execute(std::vector<TRITONBACKEND_Request*>& triton_requests);
+
+  class TritonBackendThread {
+   public:
+    static Status CreateBackendThread(
+        const std::string name, TritonModel* model, const int nice,
+        const int32_t device_id,
+        std::unique_ptr<TritonBackendThread>* triton_backend_thread);
+    Status AddModelInstance(TritonModelInstance* model_instance);
+    ~TritonBackendThread();
+
+   private:
+    TritonBackendThread(const std::string& name, TritonModel* model);
+    void BackendThread(const int nice, const int32_t device_id);
+
+    std::string name_;
+
+    TritonModel* model_;
+    std::vector<TritonModelInstance*> model_instances_;
+
+    std::thread backend_thread_;
+    std::atomic<bool> backend_thread_exit_;
+  };
+  std::shared_ptr<TritonBackendThread> triton_backend_thread_;
+
+  struct WarmupData {
+    WarmupData(const std::string& sample_name) : sample_name_(sample_name) {}
+
+    std::string sample_name_;
+    std::vector<std::unique_ptr<InferenceRequest>> requests_;
+
+    // Placeholder for input data
+    std::unique_ptr<AllocatedMemory> zero_data_;
+    std::unique_ptr<AllocatedMemory> random_data_;
+    std::vector<std::unique_ptr<std::string>> provided_data_;
+  };
+  std::vector<WarmupData> warmup_samples_;
 
   // The TritonModel object that owns this instance. The instance
   // holds this as a raw pointer because the lifetime of the model is
@@ -98,6 +158,8 @@ class TritonModelInstance {
   TritonServerMessage host_policy_message_;
   std::vector<std::string> profile_names_;
   bool passive_;
+
+  std::shared_ptr<TritonBackendThread> backend_thread_;
 
   // Reporter for metrics, or nullptr if no metrics should be reported
   std::shared_ptr<MetricModelReporter> reporter_;

src/servers/main.cc
@@ -241,6 +241,8 @@ enum OptionId {
   OPTION_MODEL_CONTROL_MODE,
   OPTION_POLL_REPO_SECS,
   OPTION_STARTUP_MODEL,
+  OPTION_RATE_LIMIT,
+  OPTION_RATE_LIMIT_RESOURCE,
   OPTION_PINNED_MEMORY_POOL_BYTE_SIZE,
   OPTION_CUDA_MEMORY_POOL_BYTE_SIZE,
   OPTION_MIN_SUPPORTED_COMPUTE_CAPABILITY,
@@ -400,6 +402,27 @@ std::vector<Option> options_
        "Name of the model to be loaded on server startup. It may be specified "
        "multiple times to add multiple models. Note that this option will only "
        "take affect if --model-control-mode=explicit is true."},
+      {OPTION_RATE_LIMIT, "rate-limit", Option::ArgStr,
+       "Specify the mode for rate limiting. Options are \"execution_count\" "
+       "and \"off\". The default is \"execution_count\". For "
+       "\"execution_count\", the server will determine the instance using "
+       "configured priority and the number of time the instance has been "
+       "used to run inference. The inference will finally be executed once "
+       "the required resources are available. For \"off\", the server will "
+       "ignore any rate limiter config and run inference as soon as an "
+       "instance is ready."},
+      {OPTION_RATE_LIMIT_RESOURCE, "rate-limit-resource",
+       "<string>:<integer>:<integer>",
+       "The number of resources available to the server. The format of this "
+       "flag is --rate-limit-resource=<resource_name>:<count>:<device>. The "
+       "<device> is optional and if not listed will be applied to every "
+       "device. If the resource is specified as \"GLOBAL\" in the model "
+       "configuration the resource is considered shared among all the devices "
+       "in the system. The <device> property is ignored for such resources. "
+       "This flag can be specified multiple times to specify each resources "
+       "and their availability. By default, the max across all instances that "
+       "list the resource is selected as its availability. The values for this "
+       "flag is case-insensitive."},
       {OPTION_PINNED_MEMORY_POOL_BYTE_SIZE, "pinned-memory-pool-byte-size",
        Option::ArgInt,
        "The total byte size that can be allocated as pinned system memory. "
@@ -929,6 +952,45 @@ ParseTraceLevelOption(std::string arg)
 }
 #endif  // TRITON_ENABLE_TRACING
 
+std::tuple<std::string, int, int>
+ParseRateLimiterResourceOption(const std::string arg)
+{
+  std::string error_string(
+      "--rate-limit-resource option format is "
+      "'<resource_name>:<count>:<device>' or '<resource_name>:<count>'. Got " +
+      arg);
+
+  std::string name_string("");
+  int count = -1;
+  int device_id = -1;
+
+  size_t delim_first = arg.find(":");
+  size_t delim_second = arg.find(":", delim_first + 1);
+
+  if (delim_second != std::string::npos) {
+    // Handle format `<resource_name>:<count>:<device>'
+    size_t delim_third = arg.find(":", delim_second + 1);
+    if (delim_third != std::string::npos) {
+      std::cerr << error_string << std::endl;
+      exit(1);
+    }
+    name_string = arg.substr(0, delim_first);
+    count = ParseIntOption(
+        arg.substr(delim_first + 1, delim_second - delim_first - 1));
+    device_id = ParseIntOption(arg.substr(delim_second + 1));
+  } else if (delim_first != std::string::npos) {
+    // Handle format `<resource_name>:<count>'
+    name_string = arg.substr(0, delim_first);
+    count = ParseIntOption(arg.substr(delim_first + 1));
+  } else {
+    // If no colons found
+    std::cerr << error_string << std::endl;
+    exit(1);
+  }
+
+  return {name_string, count, device_id};
+}
+
 std::tuple<std::string, std::string, std::string>
 ParseBackendConfigOption(const std::string arg)
 {
@@ -1072,6 +1134,10 @@ Parse(TRITONSERVER_ServerOptions** server_options, int argc, char** argv)
 
   TRITONSERVER_ModelControlMode control_mode = TRITONSERVER_MODEL_CONTROL_NONE;
   std::set<std::string> startup_models_;
+
+  TRITONSERVER_RateLimitMode rate_limit_mode =
+      TRITONSERVER_RATE_LIMIT_EXEC_COUNT;
+  std::vector<std::tuple<std::string, int, int>> rate_limit_resources;
 
 #ifdef TRITON_ENABLE_LOGGING
   bool log_info = true;
@@ -1249,6 +1315,40 @@ Parse(TRITONSERVER_ServerOptions** server_options, int argc, char** argv)
         }
         break;
       }
+      case OPTION_RATE_LIMIT: {
+        std::string rate_limit_str(optarg);
+        std::transform(
+            rate_limit_str.begin(), rate_limit_str.end(),
+            rate_limit_str.begin(), ::tolower);
+        if (rate_limit_str == "execution_count") {
+          rate_limit_mode = TRITONSERVER_RATE_LIMIT_EXEC_COUNT;
+        } else if (rate_limit_str == "off") {
+          rate_limit_mode = TRITONSERVER_RATE_LIMIT_OFF;
+        } else {
+          std::cerr << "invalid argument for --rate-limit" << std::endl;
+          std::cerr << Usage() << std::endl;
+          return false;
+        }
+        break;
+      }
+      case OPTION_RATE_LIMIT_RESOURCE: {
+        std::string rate_limit_resource_str(optarg);
+        std::transform(
+            rate_limit_resource_str.begin(), rate_limit_resource_str.end(),
+            rate_limit_resource_str.begin(), ::tolower);
+        try {
+          rate_limit_resources.push_back(
+              ParseRateLimiterResourceOption(optarg));
+        }
+        catch (const std::invalid_argument& ia) {
+          return TRITONSERVER_ErrorNew(
+              TRITONSERVER_ERROR_INVALID_ARG,
+              (std::string("failed to parse '") + optarg +
+               "' as <str>:<int>:<int>")
+                  .c_str());
+        }
+        break;
+      }
       case OPTION_PINNED_MEMORY_POOL_BYTE_SIZE:
         pinned_memory_pool_byte_size = ParseLongLongOption(optarg);
         break;
@@ -1354,6 +1454,16 @@ Parse(TRITONSERVER_ServerOptions** server_options, int argc, char** argv)
     FAIL_IF_ERR(
         TRITONSERVER_ServerOptionsSetStartupModel(loptions, model.c_str()),
         "setting startup model");
+  }
+  FAIL_IF_ERR(
+      TRITONSERVER_ServerOptionsSetRateLimiterMode(loptions, rate_limit_mode),
+      "setting rate limiter configuration");
+  for (const auto& resource : rate_limit_resources) {
+    FAIL_IF_ERR(
+        TRITONSERVER_ServerOptionsAddRateLimiterResource(
+            loptions, std::get<0>(resource).c_str(), std::get<1>(resource),
+            std::get<2>(resource)),
+        "setting rate limiter resource");
   }
   FAIL_IF_ERR(
       TRITONSERVER_ServerOptionsSetPinnedMemoryPoolByteSize(

NVIDIA/triton-inference-server

src/backends/backend/triton_model_instance.h
@@ -25,17 +25,23 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
+#include <functional>
+#include <future>
 #include <memory>
 #include <string>
+#include <thread>
 #include "model_config.pb.h"
 #include "src/core/constants.h"
+#include "src/core/memory.h"
 #include "src/core/metric_model_reporter.h"
 #include "src/core/server_message.h"
 #include "src/core/status.h"
+#include "triton/common/sync_queue.h"
 
 namespace nvidia { namespace inferenceserver {
 
 class TritonModel;
+class InferenceRequest;
 
 //
 // Represents a model instance.
@@ -44,7 +50,7 @@ class TritonModelInstance {
  public:
   static Status CreateInstances(
       TritonModel* model, const HostPolicyCmdlineConfigMap& host_policy_map,
-      const inference::ModelConfig& model_config);
+      const inference::ModelConfig& model_config, const bool device_blocking);
   ~TritonModelInstance();
 
   const std::string& Name() const { return name_; }
@@ -59,7 +65,13 @@ class TritonModelInstance {
   bool IsPassive() const { return passive_; }
   const std::vector<std::string>& Profiles() const { return profile_names_; }
 
-  TritonModel* Model() { return model_; }
+  Status Initialize();
+  Status WarmUp();
+  void Schedule(
+      std::vector<std::unique_ptr<InferenceRequest>>&& requests,
+      const std::function<void()>& OnCompletion);
+
+  TritonModel* Model() const { return model_; }
   void* State() { return state_; }
   void SetState(void* state) { state_ = state; }
 
@@ -67,7 +79,7 @@ class TritonModelInstance {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(TritonModelInstance);
-
+  class TritonBackendThread;
   TritonModelInstance(
       TritonModel* model, const std::string& name, const size_t index,
       const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
@@ -79,7 +91,56 @@ class TritonModelInstance {
       const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
       const std::vector<std::string>& profile_names, const bool passive,
       const std::string& host_policy_name,
-      const HostPolicyCmdlineConfig& host_policy);
+      const HostPolicyCmdlineConfig& host_policy,
+      const inference::ModelRateLimiter& rate_limiter_config,
+      const bool device_blocking,
+      std::map<uint32_t, std::shared_ptr<TritonBackendThread>>*
+          device_to_thread_map);
+  Status SetBackendThread(
+      const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
+      const bool device_blocking,
+      std::map<uint32_t, std::shared_ptr<TritonBackendThread>>*
+          device_to_thread_map);
+  Status GenerateWarmupData();
+
+  void Execute(std::vector<TRITONBACKEND_Request*>& triton_requests);
+
+  class TritonBackendThread {
+   public:
+    static Status CreateBackendThread(
+        const std::string name, TritonModelInstance* model, const int nice,
+        const int32_t device_id,
+        std::unique_ptr<TritonBackendThread>* triton_backend_thread);
+    void AddModelInstance(TritonModelInstance* model_instance);
+    Status InitAndWarmUpModelInstance(TritonModelInstance* model_instance);
+    ~TritonBackendThread();
+
+   private:
+    TritonBackendThread(const std::string& name, TritonModel* model);
+    void BackendThread(const int nice, const int32_t device_id);
+
+    std::string name_;
+
+    TritonModel* model_;
+    std::deque<TritonModelInstance*> model_instances_;
+
+    std::thread backend_thread_;
+    std::atomic<bool> backend_thread_exit_;
+  };
+  std::shared_ptr<TritonBackendThread> triton_backend_thread_;
+
+  struct WarmupData {
+    WarmupData(const std::string& sample_name) : sample_name_(sample_name) {}
+
+    std::string sample_name_;
+    std::vector<std::unique_ptr<InferenceRequest>> requests_;
+
+    // Placeholder for input data
+    std::unique_ptr<AllocatedMemory> zero_data_;
+    std::unique_ptr<AllocatedMemory> random_data_;
+    std::vector<std::unique_ptr<std::string>> provided_data_;
+  };
+  std::vector<WarmupData> warmup_samples_;
 
   // The TritonModel object that owns this instance. The instance
   // holds this as a raw pointer because the lifetime of the model is
@@ -98,6 +159,8 @@ class TritonModelInstance {
   TritonServerMessage host_policy_message_;
   std::vector<std::string> profile_names_;
   bool passive_;
+
+  std::shared_ptr<TritonBackendThread> backend_thread_;
 
   // Reporter for metrics, or nullptr if no metrics should be reported
   std::shared_ptr<MetricModelReporter> reporter_;

src/servers/main.cc
@@ -249,6 +249,8 @@ enum OptionId {
   OPTION_MODEL_CONTROL_MODE,
   OPTION_POLL_REPO_SECS,
   OPTION_STARTUP_MODEL,
+  OPTION_RATE_LIMIT,
+  OPTION_RATE_LIMIT_RESOURCE,
   OPTION_PINNED_MEMORY_POOL_BYTE_SIZE,
   OPTION_CUDA_MEMORY_POOL_BYTE_SIZE,
   OPTION_MIN_SUPPORTED_COMPUTE_CAPABILITY,
@@ -440,6 +442,28 @@ std::vector<Option> options_
        "Name of the model to be loaded on server startup. It may be specified "
        "multiple times to add multiple models. Note that this option will only "
        "take affect if --model-control-mode=explicit is true."},
+      // FIXME:  fix the default to execution_count once RL logic is complete.
+      {OPTION_RATE_LIMIT, "rate-limit", Option::ArgStr,
+       "Specify the mode for rate limiting. Options are \"execution_count\" "
+       "and \"off\". The default is \"off\". For "
+       "\"execution_count\", the server will determine the instance using "
+       "configured priority and the number of time the instance has been "
+       "used to run inference. The inference will finally be executed once "
+       "the required resources are available. For \"off\", the server will "
+       "ignore any rate limiter config and run inference as soon as an "
+       "instance is ready."},
+      {OPTION_RATE_LIMIT_RESOURCE, "rate-limit-resource",
+       "<string>:<integer>:<integer>",
+       "The number of resources available to the server. The format of this "
+       "flag is --rate-limit-resource=<resource_name>:<count>:<device>. The "
+       "<device> is optional and if not listed will be applied to every "
+       "device. If the resource is specified as \"GLOBAL\" in the model "
+       "configuration the resource is considered shared among all the devices "
+       "in the system. The <device> property is ignored for such resources. "
+       "This flag can be specified multiple times to specify each resources "
+       "and their availability. By default, the max across all instances that "
+       "list the resource is selected as its availability. The values for this "
+       "flag is case-insensitive."},
       {OPTION_PINNED_MEMORY_POOL_BYTE_SIZE, "pinned-memory-pool-byte-size",
        Option::ArgInt,
        "The total byte size that can be allocated as pinned system memory. "
@@ -969,6 +993,45 @@ ParseTraceLevelOption(std::string arg)
 }
 #endif  // TRITON_ENABLE_TRACING
 
+std::tuple<std::string, int, int>
+ParseRateLimiterResourceOption(const std::string arg)
+{
+  std::string error_string(
+      "--rate-limit-resource option format is "
+      "'<resource_name>:<count>:<device>' or '<resource_name>:<count>'. Got " +
+      arg);
+
+  std::string name_string("");
+  int count = -1;
+  int device_id = -1;
+
+  size_t delim_first = arg.find(":");
+  size_t delim_second = arg.find(":", delim_first + 1);
+
+  if (delim_second != std::string::npos) {
+    // Handle format `<resource_name>:<count>:<device>'
+    size_t delim_third = arg.find(":", delim_second + 1);
+    if (delim_third != std::string::npos) {
+      std::cerr << error_string << std::endl;
+      exit(1);
+    }
+    name_string = arg.substr(0, delim_first);
+    count = ParseIntOption(
+        arg.substr(delim_first + 1, delim_second - delim_first - 1));
+    device_id = ParseIntOption(arg.substr(delim_second + 1));
+  } else if (delim_first != std::string::npos) {
+    // Handle format `<resource_name>:<count>'
+    name_string = arg.substr(0, delim_first);
+    count = ParseIntOption(arg.substr(delim_first + 1));
+  } else {
+    // If no colons found
+    std::cerr << error_string << std::endl;
+    exit(1);
+  }
+
+  return {name_string, count, device_id};
+}
+
 std::tuple<std::string, std::string, std::string>
 ParseBackendConfigOption(const std::string arg)
 {
@@ -1112,6 +1175,13 @@ Parse(TRITONSERVER_ServerOptions** server_options, int argc, char** argv)
 
   TRITONSERVER_ModelControlMode control_mode = TRITONSERVER_MODEL_CONTROL_NONE;
   std::set<std::string> startup_models_;
+
+  // FIXME: Once the rate limiter implementation is complete make
+  // EXEC_COUNT the default.
+  // TRITONSERVER_RateLimitMode rate_limit_mode =
+  //    TRITONSERVER_RATE_LIMIT_EXEC_COUNT;
+  TRITONSERVER_RateLimitMode rate_limit_mode = TRITONSERVER_RATE_LIMIT_OFF;
+  std::vector<std::tuple<std::string, int, int>> rate_limit_resources;
 
 #ifdef TRITON_ENABLE_LOGGING
   bool log_info = true;
@@ -1310,6 +1380,40 @@ Parse(TRITONSERVER_ServerOptions** server_options, int argc, char** argv)
         }
         break;
       }
+      case OPTION_RATE_LIMIT: {
+        std::string rate_limit_str(optarg);
+        std::transform(
+            rate_limit_str.begin(), rate_limit_str.end(),
+            rate_limit_str.begin(), ::tolower);
+        if (rate_limit_str == "execution_count") {
+          rate_limit_mode = TRITONSERVER_RATE_LIMIT_EXEC_COUNT;
+        } else if (rate_limit_str == "off") {
+          rate_limit_mode = TRITONSERVER_RATE_LIMIT_OFF;
+        } else {
+          std::cerr << "invalid argument for --rate-limit" << std::endl;
+          std::cerr << Usage() << std::endl;
+          return false;
+        }
+        break;
+      }
+      case OPTION_RATE_LIMIT_RESOURCE: {
+        std::string rate_limit_resource_str(optarg);
+        std::transform(
+            rate_limit_resource_str.begin(), rate_limit_resource_str.end(),
+            rate_limit_resource_str.begin(), ::tolower);
+        try {
+          rate_limit_resources.push_back(
+              ParseRateLimiterResourceOption(optarg));
+        }
+        catch (const std::invalid_argument& ia) {
+          return TRITONSERVER_ErrorNew(
+              TRITONSERVER_ERROR_INVALID_ARG,
+              (std::string("failed to parse '") + optarg +
+               "' as <str>:<int>:<int>")
+                  .c_str());
+        }
+        break;
+      }
       case OPTION_PINNED_MEMORY_POOL_BYTE_SIZE:
         pinned_memory_pool_byte_size = ParseLongLongOption(optarg);
         break;
@@ -1415,6 +1519,16 @@ Parse(TRITONSERVER_ServerOptions** server_options, int argc, char** argv)
     FAIL_IF_ERR(
         TRITONSERVER_ServerOptionsSetStartupModel(loptions, model.c_str()),
         "setting startup model");
+  }
+  FAIL_IF_ERR(
+      TRITONSERVER_ServerOptionsSetRateLimiterMode(loptions, rate_limit_mode),
+      "setting rate limiter configuration");
+  for (const auto& resource : rate_limit_resources) {
+    FAIL_IF_ERR(
+        TRITONSERVER_ServerOptionsAddRateLimiterResource(
+            loptions, std::get<0>(resource).c_str(), std::get<1>(resource),
+            std::get<2>(resource)),
+        "setting rate limiter resource");
   }
   FAIL_IF_ERR(
       TRITONSERVER_ServerOptionsSetPinnedMemoryPoolByteSize(